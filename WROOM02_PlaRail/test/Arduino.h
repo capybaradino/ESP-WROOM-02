c680dbe3cb8e62ea72cc30b05c064589c885ec7e
capybaradino <20378644+capybaradino@users.noreply.github.com>
2018-02-26 00:20:36 +0900

    implement arduino std lib related function

--- WROOM02_PlaRail/test/Arduino.h
+++ WROOM02_PlaRail/test/Arduino.h
@@ -3,11 +3,24 @@
 #define HIGH (1)
 #define LOW (0)
 #define OUTPUT (1)
+#define INPUT (0)
 
 void digitalWrite(int PINNO, int HIGHLOW){
+	std::string str = "";
+	switch(HIGHLOW)
+	{
+		case HIGH:
+		str += "HIGH"; break;
+		case LOW:
+		str += "LOW"; break;
+		default:
+		str += "UNKNOWN"; break;
+	}
+	std::cout << "[DBG] digitalWrite(" << PINNO << ", " << str << ") called\n";
 }
 
 void delay(int TIME){
+	std::cout << "[DBG] delay(" << TIME << ") called\n";
 }
 
 class Serial
@@ -28,18 +41,32 @@ void Serial::println(const char* str)
 
 void Serial::println()
 {
+	std::cout << "\n";
 }
 
 void Serial::print(const char* str)
 {
+	std::cout << str;
 }
 
 void Serial::begin(int rate)
 {
+	std::cout << "[DBG] Serial::begin(" << rate << ") called\n";
 }
 
 void pinMode(int PINNO, int DIRECTION)
 {
+	std::string str = "";
+	switch(DIRECTION)
+	{
+		case OUTPUT:
+		str += "OUTPUT"; break;
+		case INPUT:
+		str += "INPUT"; break;
+		default:
+		str += "UNKNOWN"; break;
+	}
+	std::cout << "[DBG] pinMode(" << PINNO << ", " + str + ") called\n";
 }
 
 Serial Serial;
